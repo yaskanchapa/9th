63d1dc795a4205ae9723316d74918eecae34ba3e
CCPAPA <yaskanchapa@yahoo.co.jp>
2021-08-21 18:56:00 +0900

    java0821

---
+++ JAVA data/day5/List_3.java
@@ -0,0 +1,36 @@
+package day5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List_3 {
+
+	public static void main(String[] args) {
+
+//		int 는 Integer 쓰고 불린은 Boolean 쓰면됨
+//		List<Integer> ageList = new ArrayList<Integer>();
+//		List<Boolean> ageList = new ArrayList<Boolean>();
+
+		List<Integer> ageList = new ArrayList<Integer>();
+		ageList.add(13);
+		ageList.add(23);
+		ageList.add(43);
+
+		for(int age : ageList) {
+			System.out.println(age);
+		}
+
+		System.out.println("1에서 1만까지 담고 출력");
+		List<Integer> numList = new ArrayList<Integer>();
+
+		for(int i = 1; i<10001; i++) {
+			numList.add(i);
+		}
+
+		for(int num : numList) {
+			System.out.println(num);
+		}
+
+	}
+
+}
